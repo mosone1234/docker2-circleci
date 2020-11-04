549dbc45c697a846f37a1181cd15cb0fcbe82617
stone <xhosone@gmail.com>
2020-11-04 19:42:05 +0000

    Fixde bug

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /home/src/docker2-circleci
-sed -i "s/hola-docker:.*/hola-docker:$1/g" docker-compose-override.yml
+sed -i "s/hola-docker:.*/hola-docker:$1/g" docker-compose.override.yml
 docker-compose up -d
